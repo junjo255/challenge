05c8d4922de7da1a2d9f739d900cb072600d9bc5
David Yoon <david_yoon86@hotmail.com>
2020-06-17 15:31:16 -0400

    loadBalancer

--- Companies/Amazon/LoadBalancer [OA]/loadBalancer.js
+++ Companies/Amazon/LoadBalancer [OA]/loadBalancer.js
@@ -11,3 +11,27 @@
      Input:  array =[1, 1, 1, 1],
      Output: false
  */
+
+ let loadBalancer = array => {
+ 	let balanced = false;
+ 	if(array.length < 5) {
+ 		return false;
+ 	}
+
+ 	for(let i = 1; i < array.length - 4; i++) {
+ 		for(let j = i + 2; j < array.length - 2; j++) {
+ 			let firstBlock = array.slice(0, i).reduce((a, b) => a + b, 0);
+ 			let secondBlock = array.slice(i+1, j).reduce((a, b) => a + b, 0);
+ 			let thirdBlock = array.slice(j+1, array.length).reduce((a, b) => a + b, 0);;
+
+ 			if(firstBlock === secondBlock && secondBlock === thirdBlock) {
+ 				balanced = true;
+ 				break;
+ 			}
+ 		}
+ 		if(balanced) {
+ 			break;
+ 		}
+ 	}
+ 	return balanced;
+ }
