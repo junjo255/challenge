fc9066d9c9f423a60cd43a15953bc8451ac6fd23
David Yoon <david_yoon86@hotmail.com>
2020-06-30 20:00:50 -0400

    characterFrequency

--- characterFrequency/characterFrequency.js
+++ characterFrequency/characterFrequency.js
@@ -37,5 +37,41 @@
 
 
 var characterFrequency = function(string) {
+	let collection = {};
+	let result = [];
+	let swapped = true;
+	let temp;
+
+	string.split('').forEach(letter => {
+		letter in collection ? collection[letter] = collection[letter] + 1 : collection[letter] = 1;
+	});
+
+	for(let key in collection) {
+		result.push([key, collection[key]])
+	} 
+
+	while(swapped === true) {
+		swapped = false;
+
+		for(let i = result.length - 1; i > 0; i--) {
+			if(result[i][1] > result[i - 1][1] || result[i][1] > result[i - 1][1] && result[i][0].charCodeAt(0) < result[i - 1][0].charCodeAt(0)) {
+				temp = result[i];
+				result[i] = result[i - 1];
+				result[i - 1] = temp;
+        swapped = true;
+			}
+		}
+	}
+
   return result;
 };
+
+
+
+
+
+
+
+
+
+
