3d5b309532559c7049a1bc7775fdb261676205bc
Eric Jo <43450544+junjo255@users.noreply.github.com>
2020-07-09 06:32:17 -0400

    wip: findInMountainArrays

--- src/main/findInMountainArray/findInMountainArray.kt
+++ src/main/findInMountainArray/findInMountainArray.kt
@@ -1,21 +1,26 @@
+package main.findInMountainArray
 /**
+1095. Find in Mountain Array
+
 (This problem is an interactive problem.)
 
 You may recall that an array A is a mountain array if and only if:
 
-A.length >= 3
-There exists some i with 0 < i < A.length - 1 such that:
-A[0] < A[1] < ... A[i-1] < A[i]
-A[i] > A[i+1] > ... > A[A.length - 1]
-Given a mountain array mountainArr, return the minimum index such that mountainArr.get(index) == target.  If such an index doesn't exist, return -1.
+- A.length >= 3
+- There exists some i with 0 < i < A.length - 1 such that:
+	    - A[0] < A[1] < ... A[i-1] < A[i]
+		- A[i] > A[i+1] > ... > A[A.length - 1]
+
+Given a mountain array mountainArr, return the minimum index such that mountainArr.get(index) == target.
+If such an index doesn't exist, return -1.
 
 You can't access the mountain array directly.  You may only access the array using a MountainArray interface:
 
 MountainArray.get(k) returns the element of the array at index k (0-indexed).
 MountainArray.length() returns the length of the array.
-Submissions making more than 100 calls to MountainArray.get will be judged Wrong Answer.  Also, any solutions that attempt to circumvent the judge will result in disqualification.
 
- 
+Submissions making more than 100 calls to MountainArray.get will be judged Wrong Answer.
+Also, any solutions that attempt to circumvent the judge will result in disqualification.
 
 Example 1:
 
@@ -36,3 +41,12 @@ Constraints:
 0 <= mountain_arr.get(index) <= 10^9
 
 **/
+
+fun main() {
+
+
+}
+
+//fun findInMountainArray(target: Int, mountainArr: MountainArray): Int {
+//
+//}
