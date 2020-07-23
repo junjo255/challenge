224aaa552c1b595320d0ab1eac78a60e00e1fbf6
Eric Jo <43450544+junjo255@users.noreply.github.com>
2020-07-23 06:48:42 -0400

    strongpassword

--- src/main/strongPassword/strongPassword.kt
+++ src/main/strongPassword/strongPassword.kt
@@ -1,4 +1,7 @@
 package main.strongPassword
+
+import kotlin.math.min
+
 /**
 https://www.hackerrank.com/challenges/strong-password/problem
 Louise joined a social networking site to stay in touch with her friends. The signup page required her to
@@ -20,22 +23,40 @@ upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 special_characters = "!@#$%^&*()-+"
  */
 
-
 fun main() {
-
+    println(minimumNumber(3, "Ab1")) // output: 3
+    println(minimumNumber(11, "#HackerRank")) // output: 1
+    println(minimumNumber(4, "4700")) // output: 3
+    println(minimumNumber(4, "goxg")) // output: 3
+    println(minimumNumber(7, "AUzs-nV")) // output: 1
 }
 
-//fun minimumNumber(
-//        n: Int,
-//        password: String
-//): Int {
-//    var count = 0
-//
-//    if (n > 6) {
-//        return n - 6
-//    } else {
-//        password.let {
-//            if (it.contains )
-//        }
-//    }
-//}
+fun minimumNumber (
+        n: Int,
+        password: String
+): Int {
+    var count = 0
+    val specialChar = Regex("[!@#$%^&*()\\-+]")
+    val lowerCase = Regex("[a-z]")
+    val upperCase = Regex("[A-Z]")
+    val numbers = Regex("[0-9]")
+
+    var specialCharFlag = false
+    var lowerCaseFlag = false
+    var upperCaseFlag = false
+    var numbersFlag = false
+
+    password.let {
+        if (specialChar.containsMatchIn(it)) specialCharFlag = true
+        if (lowerCase.containsMatchIn(it)) lowerCaseFlag = true
+        if (upperCase.containsMatchIn(it)) upperCaseFlag = true
+        if (numbers.containsMatchIn(it)) numbersFlag = true
+
+        if (!specialCharFlag) count++
+        if (!lowerCaseFlag) count++
+        if (!upperCaseFlag) count++
+        if (!numbersFlag) count++
+
+        return if ((n < 6) && (n + count > 6)) count else if ((n < 6) && (n + count <= 6)) 6 - n else count
+    }
+}
